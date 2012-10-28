ef6fa5af0947de296162bf3ccccca71832af41a9
ratancs <mail@ratan.co.cc>
2012-10-28 15:59:47 +0530

    #16 completed

--- p_16.c
+++ p_16.c
@@ -1,3 +1,11 @@
+/*
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 void main()
